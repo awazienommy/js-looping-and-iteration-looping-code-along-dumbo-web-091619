b58a6dc175701f309791bd6ce73649440cdaa386
awazienommy <coawazie@gmail.com>
2019-10-27 19:12:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const namedPeople = [[ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise']
+
+function writeCards(namedPeople) {
+
+    let output = []
+    let first = ['Lisa', 'Kaitlin', 'Jan' ]
+    let response = "surprise"
+    for (let i = 0; i < first.length; i++ ) {
+        
+        output.push(`Thank you, ${first[i]}, for the wonderful ${response} gift!`);
+
+}
+
+return output;
+}
+
+function countDown(number) {
+
+    while ( number > 0 ) {
+        console.log(number);
+        number -= 1;
+    }
+
+    console.log(number);
+
+
+
+}
